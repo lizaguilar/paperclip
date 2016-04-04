064bcb02225f898097aff5eb39cc04c72e055836
elizabeth aguilar <elakeys@gmail.com>
2016-04-04 14:39:13 -0400

    added daniel as default user avatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@
 class User < ActiveRecord::Base
   has_attached_file :avatar, :styles =>
   { :medium => "300x300>", :thumb => "100x100>" },
-  :default_url => ":style/missing.png"
+  :default_url => "/images/dan.png"
   validates_attachment_content_type :avatar,
   :content_type => /\Aimage\/.*\Z/
 end
